96975bde06a0964c20ccffd7c23da835bcf7401e
Franz Pretorius <franzj.pretorius.2003@gmail.com>
2024-08-26 16:49:35 +0200

    Finished about me section

--- src/components/About_Me.jsx
+++ src/components/About_Me.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 import './Components.css'; // Import the CSS file for styling
 
 const About_Me = () => {
-  return (
-    <div className="about-me" id="about-me">
-        
-    </div>
-  )
-};
+    return (
+        <>
+        <div className="about-me" id="about-me">
+            <h1>About me</h1>
+            <br />
+            <div className="about-me-heading">
+                <h1>Who I am</h1>
+                <h2>Designer and Developer based in Johannesburg, South Africa</h2>
+                <p>Hi, I'm Franz Pretorius, a front-end developer with a creative flair that's always a cut above the rest! I've got a solid foundation in HTML, CSS, and JavaScript, and I'm always styling my skills with the latest trends.</p>
+                <p>After branching out from the University of Johannesburg, I committed to a 6-month Web Development course at CodeSpace Academy, where I merged my knowledge with hands-on experience in TypeScript, Tailwind CSS, React, Bootstrap, Framer-Motion and version control tools like Git and GitHub. I'm currently upskilling in UI/UX design using Figma.</p>
+                <p>My portfolio is loaded with projects that showcase my skills, from a bank landing page to a kanban task management application, and even a podcast app that streams online content using the Fetch API.</p>
+                <p>I'm a team player who's always ready to lend a hand and collaborate on projects. My debugging skills are on point, and I'm always adapting to new development tasks.</p>
+                <p>As a dedicated problem-solver, I thrive on resolving issues and completing projects to the highest standard. If you're looking for a reliable, enthusiastic front-end developer who's always connected and ready to push the boundaries, let's link up and create something amazing together!</p>
+            </div>
+        </div>
+        </>
+      );
+    };
 
 export default About_Me;

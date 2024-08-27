ff60d7f2c9eeb078c1618778910c2e23bc0ca4b9
Franz Pretorius <franzj.pretorius.2003@gmail.com>
2024-08-27 13:39:23 +0200

    Added project images and some styling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,31 +1,36 @@
 import React, { useEffect } from 'react';
 import './Components.css'; // Import the CSS file for styling
 import './Animated_bg.css';
+import podd from '../assets/Podd.png'
+import kanban from '../assets/Kanban.png'
+import cache from '../assets/CacheBank.png'
 
 const Projects = () => {
     return (
         <>
-            <div className="inbetween-fade"></div>
+            <div className="inbetween-fade-begin"></div>
             <div className="stars-div">
                 <div id="stars"></div>
                 <div id="stars2"></div>
                 <div id="stars3"></div>
                 <div className="projects">
+                    <h1 className="projects-title">Projects</h1>
                     <div className="project">
-                        <img></img>
+                        <img src={podd}></img>
                         <p></p>
                     </div>
                     <div className="project">
-                        <img></img>
+                        <img src={kanban}></img>
                         <p></p>
                     </div>
                     <div className="project">
-                        <img></img>
+                        <img src={cache}></img>
                         <p></p>
                     </div>
                     
                 </div>
             </div>
+            <div className="inbetween-fade-end"></div>
             <div className='placeholder'></div>
         </>
       );

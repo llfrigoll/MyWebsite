da3a20782f60a5118f85fa30e2eceb886f4639f3
Franz Pretorius <franzj.pretorius.2003@gmail.com>
2024-08-28 15:08:35 +0200

    Updated link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <div className="footer" id="footer">
             <a href="https://github.com/llfrigoll" target="_blank"><img className="footer-image" id="footer-image" src={github}></img></a>
-            <a href="https://linkedin.com/in/franzpretorius2003" target="_blank"><img className="footer-image" id="footer-image" src={linkedin}></img></a>
+            <a href="https://www.linkedin.com/in/franzpretorius2003/" target="_blank"><img className="footer-image" id="footer-image" src={linkedin}></img></a>
             <p>Franz Pretorius &copy;2024</p>
         </div>
       );

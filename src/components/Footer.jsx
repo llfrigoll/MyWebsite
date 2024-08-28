0e35c74509649e9a5b2249db58efba17c359d4a3
Franz Pretorius <franzj.pretorius.2003@gmail.com>
2024-08-28 15:44:01 +0200

    Added email and made tech stack smaller

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,14 @@ import React, { useEffect } from 'react';
 import './Components.css'; // Import the CSS file for styling
 import github from '../assets/Github_Logo.png'
 import linkedin from '../assets/LinkedIn_Logo.png'
+import gmail from '../assets/GMail_Logo.png'
 
 const Footer = () => {
     return (
         <div className="footer" id="footer">
             <a href="https://github.com/llfrigoll" target="_blank"><img className="footer-image" id="footer-image" src={github}></img></a>
             <a href="https://www.linkedin.com/in/franzpretorius2003/" target="_blank"><img className="footer-image" id="footer-image" src={linkedin}></img></a>
+            <a href="mailto:franzj.pretorius.2003@gmail.com"><img className="footer-image" id="footer-image" src={gmail}></img></a>
             <p>Franz Pretorius &copy;2024</p>
         </div>
       );
